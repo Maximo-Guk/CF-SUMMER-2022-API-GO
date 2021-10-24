17cd2169e25a928f848d4cc826266957bafd422d
Maximo Guk <62088388+Maximo-Guk@users.noreply.github.com>
2021-10-24 14:04:48 -0300

    Finished project, except for readme and comments

---
+++ go.mod
@@ -0,0 +1,15 @@
+module awesomeProject
+
+go 1.17
+
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/gofiber/fiber/v2 v2.20.2 // indirect
+	github.com/gofiber/jwt/v3 v3.2.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.1.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.31.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20211023085530-d6a326fbbf70 // indirect
+)
---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var numberOfAuthorizations int64 = 0
+var numberOfVerifications int64 = 0
+var sumOfAuthorizationTimes int64 = 0
+var sumOfVerificationTimes int64 = 0
+
+func getUser(userName string) (User, error) {
+	r, err := http.Get("https://ga-api.maximoguk.workers.dev/users/"+userName)
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	var user User
+	err = json.Unmarshal(b, &user)
+	return user, err
+}
+
+func main() {
+	app := fiber.New()
+
+	//auth route
+	app.Get("/auth/:userName", auth)
+
+	//verify route
+	app.Get("/verify", verify)
+
+	//stats route
+	app.Get("/stats", stats)
+
+	//readme route
+	app.Get("/README.txt", readme)
+
+	app.Listen(":4000")
+}
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type User struct {
+	Username string
+}
+
+func auth(c *fiber.Ctx) error {
+	res, err := getUser(c.Params("userName"))
+	if res.Username == "" {
+		startTime := time.Now()
+		privateBytes, err := ioutil.ReadFile("private.pem")
+		fatal(err)
+		publicBytes, err := ioutil.ReadFile("public.pem")
+		fatal(err)
+		privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateBytes)
+		fatal(err)
+
+		token := jwt.New(jwt.SigningMethodRS256)
+		claims := token.Claims.(jwt.MapClaims)
+		claims["sub"] = c.Params("userName")
+		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+		tokenString, err := token.SignedString(privateKey)
+		fatal(err)
+
+		// Create cookie
+		cookie := new(fiber.Cookie)
+		cookie.Name = "token"
+		cookie.Value = tokenString
+		// Set cookie
+		c.Cookie(cookie)
+
+		numberOfAuthorizations++
+		sumOfAuthorizationTimes += time.Since(startTime).Milliseconds()
+
+		return c.SendString(string(publicBytes))
+	} else {
+		fatal(err)
+		return c.SendString("Error, username has already been registered")
+	}
+}
+
+func verify(c *fiber.Ctx) error {
+	startTime := time.Now()
+	publicBytes, err := ioutil.ReadFile("public.pem")
+	fatal(err)
+	publicKey, err := jwt.ParseRSAPublicKeyFromPEM(publicBytes)
+	fatal(err)
+
+	tokenString := c.Cookies("token")
+	claims := jwt.MapClaims{}
+	_, err = jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return publicKey, nil
+	})
+	if err != nil {
+		return c.SendStatus(409)
+	}
+
+	numberOfVerifications++;
+	sumOfAuthorizationTimes += time.Since(startTime).Milliseconds()
+
+	return c.JSON(fiber.Map{"userName": claims["sub"]})
+}
+
+func stats(c *fiber.Ctx) error {
+	var numberOfVerificationsString = "0"
+	var averageOfVerificationsString = "n/a"
+	var numberOfAuthorizationsString = "0"
+	var averageOfAuthorizationsString = "n/a"
+
+	if numberOfVerifications != 0 {
+		numberOfVerificationsString = strconv.FormatInt(numberOfVerifications, 10)
+		averageOfVerificationsString = strconv.FormatInt(sumOfVerificationTimes/numberOfVerifications, 10)+"ms"
+	}
+
+	if numberOfAuthorizations != 0 {
+		numberOfAuthorizationsString = strconv.FormatInt(numberOfAuthorizations, 10)
+		averageOfAuthorizationsString = strconv.FormatInt(sumOfAuthorizationTimes/numberOfAuthorizations, 10)+"ms"
+	}
+
+	return c.JSON(fiber.Map{
+		"numberOfVerifications": numberOfVerificationsString,
+		"averageOfVerifications": averageOfVerificationsString,
+		"numberOfAuthorizations": numberOfAuthorizationsString,
+		"averageOfAuthorizations": averageOfAuthorizationsString,
+	})
+
+}
+
+func readme(c *fiber.Ctx) error {
+	readmeBytes, err := ioutil.ReadFile("README.txt")
+	fatal(err)
+
+	return c.SendString(string(readmeBytes))
+
+}
